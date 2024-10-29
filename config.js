114a7eb7c8c6bfe5416986fccb75d5f622d6a116
Abdur2201 <158185199+Abdur2201@users.noreply.github.com>
2024-10-29 12:57:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const config = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 10000,
     dialogflowProjectId: process.env.DIALOGFLOW_PROJECT_ID || 'fir-3a36f',
-    webhookUrl: process.env.WEBHOOK_URL || 'http://localhost:3000/webhook'
+    webhookUrl: process.env.WEBHOOK_URL || 'https://bot-backend-0y9c.onrender.com/webhook'
 };
 
 module.exports = config;
