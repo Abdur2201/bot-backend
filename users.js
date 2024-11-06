b85da108a6193c24e5551f3d7e193e8227407cca
Abdur2201 <158185199+Abdur2201@users.noreply.github.com>
2024-11-06 14:36:47 +0530

    Update users.js

--- users.js
+++ users.js
@@ -4,11 +4,11 @@ import mongoose from 'mongoose';
 
 
 const userschema=new mongoose.Schema({
-    id:{
-        type:String,
-        default:uuidv4,
-        unique:true 
-    },
+    id: {
+        type: String,
+        default: () => Math.floor(Math.random() * 900000 + 100000).toString(), 
+        unique: true 
+      },
     name:{
         type:String,
         required:true
@@ -21,16 +21,13 @@ const userschema=new mongoose.Schema({
     password:{
         type:String,
         required:true
-    }
+    },
 });
 userschema.pre('save',async function (next) {
     const user=this;
-    if(this.isNew)
-    {
-        this.id=uuidv4();
-    }
-    if(!user.isModified('password')) return next();
     
+    if(!user.isModified('password')) return next();
+
     const salt=await bcrypt.genSalt(10);
     user.password=await bcrypt.hash(user.password,salt);
     next();
@@ -39,6 +36,89 @@ userschema.pre('save',async function (next) {
 //user model !!!
 const User = mongoose.model('User', userschema);
 export default User;
+
+
+
+
+
+
+// const express=require('express');
+// const bcrypt=require('bcryptjs');
+// const { v4: uuidv4 }=require('uuid');
+// const { Mongoose, default: mongoose } = require('mongoose');
+
+// const user=mongoose.model('user',userschema);
+
+// module.exports=user;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    const hashedPassword = await bcrypt.hash(password, salt);
+
+//         const newuser = new user({
+//             email,
+//             name,
+//             password: hashedPassword  
+//         });
+//         await newuser.save();
+// import bcrypt from 'bcryptjs';
+// import { v4 as uuidv4 } from 'uuid';
+// import mongoose from 'mongoose';
+
+
+// const userschema=new mongoose.Schema({
+//     id:{
+//         type:String,
+//         default:uuidv4,
+//         unique:true 
+//     },
+//     name:{
+//         type:String,
+//         required:true
+//     },
+//     email:{
+//         type:String,
+//         required:true,
+//         unique:true
+//     },
+//     password:{
+//         type:String,
+//         required:true
+//     }
+// });
+// userschema.pre('save',async function (next) {
+//     const user=this;
+//     if(this.isNew)
+//     {
+//         this.id=uuidv4();
+//     }
+//     if(!user.isModified('password')) return next();
+    
+//     const salt=await bcrypt.genSalt(10);
+//     user.password=await bcrypt.hash(user.password,salt);
+//     next();
+// });
+
+// //user model !!!
+// const User = mongoose.model('User', userschema);
+// export default User;
 
 
 
