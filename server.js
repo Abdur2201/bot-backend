4faa09037e0b34252913290cf03b385cb44f0de6
Abdur2201 <158185199+Abdur2201@users.noreply.github.com>
2024-11-06 15:51:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/webhook', (req, res) => {
   const handleDownloadReceipt = (agent) => {
     const idNum = agent.parameters.id_num;
     if (idNum) {
-      agent.add(`Here is the download link for receipt with ID ${currentUserId}: www.google.com`);
+      agent.add(`Here is the download link for receipt with ID ${userId}: www.google.com`);
     } else {
       agent.add("Please provide a valid ID number to download the receipt.");
     }
